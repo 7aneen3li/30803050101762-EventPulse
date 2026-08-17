4f6bb390286e4defcbc3a19f0bb5f7d17dc78bfe
7aneen3li <haninali2035@gmail.com>
2026-08-17 10:51:35 +0300

    feat: add mongoose schemas for all data models

---
+++ models/registration.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const registrationSchema = new mongoose.Schema({
+    event: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Event',
+        required: true
+    },
+    attendee: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Registration', registrationSchema);
