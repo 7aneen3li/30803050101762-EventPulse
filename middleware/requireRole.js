fbae65daf7134261451ace2d80f72beb0cf63cf6
7aneen3li <haninali2035@gmail.com>
2026-08-26 01:43:20 +0300

    fix: remove stray identifier breaking role-check middleware

--- middleware/requireRole.js
+++ middleware/requireRole.js
@@ -2,7 +2,7 @@ const AppError = require('../utils/AppError');
 
 function requireRole(...roles) {
     return (req, res, next) => {
-        if (!roles.includes(req, UserActivation.role)) {
+        if (!roles.includes(req.user.role)) {
             return next(new AppError('You do not have permission to perform this action', 403));
         }
         next();
