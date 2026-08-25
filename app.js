531d83ed1a147ae81f3a050ae886ad509ef78022
7aneen3li <haninali2035@gmail.com>
2026-08-25 17:56:40 +0300

    chore: Mount event routes in app.js

--- app.js
+++ app.js
@@ -6,15 +6,16 @@ const mongoSanitize = require('express-mongo-sanitize');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/errorHandler');
 const authRoutes = require('./routes/authRoutes');
+const eventRoutes = require('./routes/eventRoutes');
 
 const app = express();
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
-
 app.use(express.json());
 app.use(mongoSanitize());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/events', eventRoutes);
 
 app.use((req, res, next) => {
   res.status(404).json({ status: 'fail', message: 'Route not found' });
